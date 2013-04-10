f74b366cd6f7d0121a7e69884ae574a2a979ed09
Nikita Orlov <n.dezz.orlov@gmail.com>
2013-04-11 01:39:04 +0400

    restructured for use as submodule

--- README.md
+++ README.md
@@ -2,3 +2,10 @@ mysimplemvc
 ===========
 
 simple mvc for light projects
+
+create .htaccess with :
+AddDefaultCharset utf-8
+RewriteEngine on
+RewriteCond %{REQUEST_URI} !^/core/init.php
+RewriteCond %{REQUEST_URI} !^.*[jpg|jpeg|gif|png|css|ico|doc|txt|html|htm]$
+RewriteRule ^(.*)$ /core/init.php [L]
--- core/init.php
+++ core/init.php
@@ -3,10 +3,57 @@
  * init.php
  * uses in .htaccess for init MVC
  * 
+ * @author Nick Dezzpil Orlov <n.dezz.orlov@gmail.com> 
  * @see .htaccess in root
  */
-include("../config/config.php");
 
+
+/**
+ * you may create your own config
+ */
+@include("../config/config.php");
+
+// Constants
+defined('HOST') || define('HOST', $_SERVER['HTTP_HOST']);
+defined('ABS_ROOT_PATH') || define('ABS_ROOT_PATH', $_SERVER['DOCUMENT_ROOT']);
+
+// Database
+defined('NO_DB_USING') || define('NO_DB_USING', false);
+defined('DB_HOST') || define("DB_HOST", "localhost");
+defined('DB_NAME') || define("DB_NAME", "");
+defined('DB_USER') || define("DB_USER", "");
+defined('DB_PASS') || define("DB_PASS", "");
+defined('DB_SALT') || define("DB_SALT", "");
+
+// relation pathes
+define('ROOT_PATH', '/');
+define('CORE_PATH', ROOT_PATH.'core/');
+define('IMAGES_PATH', ROOT_PATH.'images/');
+define('JS_PATH', ROOT_PATH.'js/');
+define('CSS_PATH', ROOT_PATH.'css/');
+
+define('TEMPLATE_PATH', ROOT_PATH.'views/template/');
+define('ZONE_PATH', ROOT_PATH.'views/zones/');
+define('MODEL_PATH', ROOT_PATH.'models/');
+define('CONTROLLER_PATH', ROOT_PATH.'controllers/');
+
+// absolute pathes
+define('ABS_CORE_PATH', ABS_ROOT_PATH.'core/');
+define('ABS_IMAGES_PATH', ABS_ROOT_PATH.'images/');
+define('ABS_MODEL_PATH', ABS_ROOT_PATH.'models/');
+define('ABS_MODUL_PATH', ABS_MODEL_PATH.'modules/');
+
+define('ABS_TEMPLATE_PATH', ABS_ROOT_PATH.'views/template/');
+define('ABS_ZONE_PATH', ABS_ROOT_PATH.'views/zones/');
+define('ABS_MODEL_PATH', ABS_ROOT_PATH.'models/');
+define('ABS_CONTROLLER_PATH', ABS_ROOT_PATH.'controllers/');
+
+define('EXT', '.php');
+
+/**
+ * include core models
+ * its all in help\ namespace
+ */
 include_once(ABS_CORE_PATH.'mvc.php');
 include_once(ABS_CORE_PATH.'model/ajax.php');
 include_once(ABS_CORE_PATH.'model/array.php');
